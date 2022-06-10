5d9a427bb5d517d851cb865be709e92da86d62d9
niels1286 <niels@nuls.io>
2022-06-10 18:30:15 +0800

    update copyright

--- nerve/converter/converter-main/src/main/java/network/nerve/converter/storage/CfmChangeBankStorageService.java
+++ nerve/converter/converter-main/src/main/java/network/nerve/converter/storage/CfmChangeBankStorageService.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  * <p>
- * Copyright (c) 2019-2020 nerve.network
+ * Copyright (c) 2019-2022 nerve.network
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- nerve/swap/src/test/java/network/nerve/swap/sender/StableSwapTxSender.java
+++ nerve/swap/src/test/java/network/nerve/swap/sender/StableSwapTxSender.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2019-2022 nerve.network
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package network.nerve.swap.sender;
 
 import io.nuls.base.basic.AddressTool;
@@ -26,8 +49,8 @@ import java.util.List;
 public class StableSwapTxSender extends ApiTxSender {
 
     private static final String apiUrl = "https://api.nerve.network/jsonrpc/";
-    private static final String prikeyHex = "75d9ef37704b7026e004af26c56a20ecf08f63f5ded37afdfa4adfa1a3f61cab";
-    private static final String address = "NERVEepb6DYfNDy22sR4NjQ8rpGRrT11Yb7tiM";
+    private static final String prikeyHex = "75d9e61ca37afd26e004af26c5d37afdf08f63f5ded37afdfa4adfa1a3f61cab";
+    private static final String address = "NERVEepb69gmzej5zNPFvwqp2cpmATczZEvrrh";
     private static final int chainId = 9;
 
     public static void main(String[] args) throws Exception {
